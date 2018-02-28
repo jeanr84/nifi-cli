41c822a8a86c0d8e4c0956624d351ef39291b908
Robin Jean <robinjeanr@gmail.com>
2018-02-28 08:52:59 +0100

    fix first NifiCompletion problems (still some missing endpoints)

--- nifi_cli/completion.py
+++ nifi_cli/completion.py
@@ -1,25 +1,29 @@
 import re
-import json
 from bs4 import BeautifulSoup
 import urllib.request
 
-class Completion:
 
-    #TODO : Make the hostname and port configurable
-    URL = "http://localhost:8000"
-    REST_API_PAGE = URL + "/nifi-docs/rest-api/index.html"
+class NifiCompletion:
 
+    def get_html_content(self):
+        # TODO : Make the hostname and port configurable
+        URL = "http://localhost:8080"
+        REST_API_PAGE = URL + "/nifi-docs/rest-api/index.html"
+
+        html = urllib.request.urlopen(REST_API_PAGE).read()
+        return BeautifulSoup(html, "html.parser")
 
     def create_tree(self):
         tree = {}
-        
-        html =  urllib.request.urlopen(REST_API_PAGE).read()
-        soup = BeautifulSoup(html, "html.parser")
-        
         p = re.compile("{.*}")
 
-        for html_endpoint in soup.findAll("div", {"class": "endpoints"}):
-            #TODO : Fix the "path hidden" part which ignore some endpoints (ex : labels)
+        try:
+            html_content = self.get_html_content()
+        except urllib.error.URLError:
+            return {}
+
+        for html_endpoint in html_content.findAll("div", {"class": "endpoints"}):
+            # TODO : Fix the "path hidden" part which ignore some endpoints (ex : labels)
             endpoint = html_endpoint.find("span", {"class": "path hidden"}).string
             method = html_endpoint.find("div", {"class": "method"}).string.lower()
             current_tree = tree
@@ -29,6 +33,6 @@ class Completion:
                     if part not in current_tree:
                         current_tree[part] = {}
                     current_tree = current_tree[part]
-            current_tree[method] =  None
+            current_tree[method] = None
 
         return tree
